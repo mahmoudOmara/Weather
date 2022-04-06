d310ddf2b66576c529bff12385a02a7e5ff0e055
Mahmoud Omara <mahmoud.omara909@gmail.com>
2022-04-06 05:52:20 +0200

    City Temperature details view model

--- Weather/View Models/CityWeatherDetailsViewModel.swift
+++ Weather/View Models/CityWeatherDetailsViewModel.swift
@@ -6,11 +6,25 @@
 //
 
 import Foundation
+import UIKit.UIImage
 
 class CityWeatherDetailsViewModel {
     
     private let city: City
     private let managedCity: ManagedCity
+    
+    private let numbersFormatter: NumberFormatter = {
+      let tempFormatter = NumberFormatter()
+      tempFormatter.numberStyle = .none
+      tempFormatter.maximumFractionDigits = 2
+      return tempFormatter
+    }()
+    
+    let icon: Box<UIImage?> = Box(nil)
+    let description = Box("")
+    let temp = Box("")
+    let humidity = Box("")
+    let wind = Box("")
     
     init(city: City, managedCity: ManagedCity) {
         self.city = city
@@ -19,7 +33,14 @@ class CityWeatherDetailsViewModel {
         OpenWeatherService.getWeatherDataForCity(self.city.name) { [weak self] weatherInfo, error in
             guard let self = self else { return }
             guard let weatherInfo = weatherInfo else { return }
-            
+            let iconURL = "http://openweathermap.org/img/w/\(weatherInfo.icon).png"
+            if let imageData = try? Data(contentsOf: URL(string: iconURL)!) {
+                self.icon.value = UIImage(data: imageData)
+            }
+            self.description.value = weatherInfo.descritption
+            self.temp.value = self.numbersFormatter.string(from: weatherInfo.temp as NSNumber) ?? ""
+            self.humidity.value = self.numbersFormatter.string(from: weatherInfo.humidity as NSNumber) ?? ""
+            self.wind.value =  self.numbersFormatter.string(from: weatherInfo.wind as NSNumber) ?? ""
         }
     }
 }
