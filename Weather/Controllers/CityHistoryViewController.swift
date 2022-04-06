e443d68947aa9076867edde9ae0c40a2f6ce6670
Mahmoud Omara <mahmoud.omara909@gmail.com>
2022-04-06 13:58:16 +0200

    CityHistoryViewController base implementation and HistoryTableViewCell

--- Weather/Controllers/CityHistoryViewController.swift
+++ Weather/Controllers/CityHistoryViewController.swift
@@ -8,17 +8,45 @@
 import UIKit
 
 class CityHistoryViewController: UIViewController {
-
+    
     static let storyBoard = UIStoryboard(name: "Weather", bundle: nil)
     static let storyBoardIdentifier = String(describing: CityHistoryViewController.self)
-
+    
+    @IBOutlet weak var historyTableView: UITableView!
+    
     //MARK:- Property Injected Items
     var viewModel: CityHistoryViewModel!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        historyTableView.register(HistoryTableViewCell.nib(), forCellReuseIdentifier: HistoryTableViewCell.reuseIdentifier)
+        historyTableView.dataSource = self
+        historyTableView.delegate = self
+    }
+    
+    @IBAction func backButtonClicked(_ sender: Any) {
+        self.navigationController?.popViewController(animated: true)
+    }
+    
+}
 
-        // Do any additional setup after loading the view.
+extension CityHistoryViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: HistoryTableViewCell.reuseIdentifier) as! HistoryTableViewCell
+        cell.configure()
+        return cell
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+
+}
+
+extension CityHistoryViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
 }
---
+++ Weather/Views/HistoryTableViewCell/HistoryTableViewCell.swift
@@ -0,0 +1,33 @@
+//
+//  HistoryTableViewCell.swift
+//  Weather
+//
+//  Created by mac on 06/04/2022.
+//
+
+import UIKit
+
+class HistoryTableViewCell: UITableViewCell {
+
+    static let reuseIdentifier = String(describing: HistoryTableViewCell.self)
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    static func nib() -> UINib {
+        return UINib(nibName: String(describing: HistoryTableViewCell.self), bundle: nil)
+    }
+    
+    func configure() {
+        
+    }
+    
+}
